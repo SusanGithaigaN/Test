37a2627f22932f34dc6709bb316834f111c6ac01
Susan Njeri Githaiga <susangithaiga26@gmail.com>
2023-07-08 11:13:56 +0300

    Added week 1 solutions

---
+++ 5-Loops/even_sum.py
@@ -0,0 +1,10 @@
+#Write your code below this row 👇
+# get all even numbers from 1 to 100
+total = 0
+ 
+for even_number in range(1, 101):
+#   print(even_number) 
+     if even_number % 2 == 0:
+        total += even_number
+print(total)    
+
---
+++ 5-Loops/range.py
@@ -0,0 +1,9 @@
+# for loop with range
+# for num in range(1, 11):
+#     print(num)
+    
+# 1 to 100
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)    
