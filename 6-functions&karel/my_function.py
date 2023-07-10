d2233263b0c50c822cc3128fda8b1ce93fbda7d2
Susan Njeri Githaiga <susangithaiga26@gmail.com>
2023-07-10 11:53:32 +0300

    Progress

---
+++ 6-functions&karel/my_function.py
@@ -0,0 +1,22 @@
+print("Hi there, Marty")
+num_char = len("Susan Njeri Githaiga")
+print(num_char)
+
+# create a new function
+
+def my_function():
+  print("Hello")
+  print("Boo Bitch!")
+
+# execute the function
+my_function()
+
+
+# creating a function in Python
+
+def new_function():
+  # Do this
+  # Then do this
+  # Finally do this
+  
+#call the function
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Test
+# This repo contains a list of all programs created during my 100 days of Python coding
